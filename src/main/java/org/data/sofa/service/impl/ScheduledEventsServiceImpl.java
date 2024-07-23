5e4329d65acd1c2c6d39564ea77349a6335e1a1c
nam.dinh <dragonsang10a1@gmail.com>
2024-07-23 16:40:04 +0700

    init

--- src/main/java/org/data/sofa/service/impl/ScheduledEventsServiceImpl.java
+++ src/main/java/org/data/sofa/service/impl/ScheduledEventsServiceImpl.java
@@ -5,6 +5,7 @@ import lombok.extern.log4j.Log4j2;
 import org.data.service.sap.SapService;
 import org.data.common.model.GenericResponseWrapper;
 import org.data.sofa.dto.ScheduledEventsCommonResponse;
+import org.data.sofa.dto.SofaScheduledEventByDateDTO;
 import org.data.sofa.dto.SofaScheduledEventsResponse;
 import org.data.sofa.repository.impl.ScheduledEventsRepository;
 import org.data.sofa.service.ScheduledEventsService;
@@ -12,6 +13,7 @@ import org.data.util.TimeUtil;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -34,8 +36,10 @@ public class ScheduledEventsServiceImpl implements ScheduledEventsService {
 		SofaScheduledEventsResponse sofaInverseScheduledEventsResponse = sapService.restSofaScoreGet(SCHEDULED_EVENTS
 				+ request.getDate() + SCHEDULED_EVENTS_INVERSE, SofaScheduledEventsResponse.class);
 
+		String date = request.getDate();
+		LocalDateTime requestDate = TimeUtil.convertStringToLocalDateTime(date);
 
-		List<EventDetail> eventDetails = getEventDetails(sofaScheduledEventsResponse, sofaInverseScheduledEventsResponse);
+		List<EventDetail> eventDetails = getEventDetails(sofaScheduledEventsResponse, sofaInverseScheduledEventsResponse, requestDate);
 
 
 		Response response = Response
@@ -52,23 +56,31 @@ public class ScheduledEventsServiceImpl implements ScheduledEventsService {
 				.build();
 	}
 
-	private @NotNull List<EventDetail> getEventDetails(SofaScheduledEventsResponse sofaScheduledEventsResponse, SofaScheduledEventsResponse sofaInverseScheduledEventsResponse) {
+	private @NotNull List<EventDetail> getEventDetails(SofaScheduledEventsResponse sofaScheduledEventsResponse,
+													   SofaScheduledEventsResponse sofaInverseScheduledEventsResponse,
+													   LocalDateTime requestDate) {
+
 		List<EventResponse> sofaScheduledEventsResponseEventResponses = sofaScheduledEventsResponse.getEvents();
 		List<EventResponse> sofaInverseScheduledEventsResponseEventResponses = sofaInverseScheduledEventsResponse.getEvents();
-
 		List<EventDetail> eventDetails = new ArrayList<>();
 
-
-		for (EventResponse responseEventResponse : sofaScheduledEventsResponseEventResponses) {
-			EventDetail eventDetail = populatedToEventDetail(responseEventResponse);
-			eventDetails.add(eventDetail);
-		}
-
-		for (EventResponse responseEventResponse : sofaInverseScheduledEventsResponseEventResponses) {
-			EventDetail eventDetail = populatedToEventDetail(responseEventResponse);
-			eventDetails.add(eventDetail);
-		}
+		getEventDetailsByDate(requestDate, sofaScheduledEventsResponseEventResponses, eventDetails);
+		getEventDetailsByDate(requestDate, sofaInverseScheduledEventsResponseEventResponses, eventDetails);
 		return eventDetails;
+	}
+
+	private void getEventDetailsByDate(LocalDateTime requestDate,
+									   List<EventResponse> sofaScheduledEventsResponseEventResponses,
+									   List<EventDetail> eventDetails) {
+		for (EventResponse responseEventResponse : sofaScheduledEventsResponseEventResponses) {
+			LocalDateTime responseDate = TimeUtil.convertUnixTimestampToLocalDateTime(responseEventResponse.getStartTimestamp());
+			if (responseDate.getDayOfMonth() == requestDate.getDayOfMonth() &&
+					responseDate.getMonth() == requestDate.getMonth() &&
+					responseDate.getYear() == requestDate.getYear()) {
+				EventDetail eventDetail = populatedToEventDetail(responseEventResponse);
+				eventDetails.add(eventDetail);
+			}
+		}
 	}
 
 
@@ -76,7 +88,6 @@ public class ScheduledEventsServiceImpl implements ScheduledEventsService {
 
 		ScheduledEventsCommonResponse.Score homeScoreResponse = eventResponse.getHomeScore();
 		ScheduledEventsCommonResponse.Score eventResponseAwayScore = eventResponse.getAwayScore();
-
 
 		return EventDetail.builder()
 				.tntName(eventResponse.getTournament().getName())
